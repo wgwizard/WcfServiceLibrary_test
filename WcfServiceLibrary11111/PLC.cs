b9ad03254e990621e72d558c5bf3c6b344adc258
Castle.Wu <Castle.Wu@topecsh.com>
2022-11-17 08:48:46 +0800

    添加项目文件。

---
+++ WcfServiceLibrary11111/PLC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary11111
+{
+    public class PLC: IPLC
+    {
+        public string GetDI(int value)
+        {
+            return string.Format("DI: {0}", value);
+        }
+
+        public Motor GetMotor(Motor _motor)
+        {
+            Motor motor = new Motor();
+            motor.MotorName ="A Line: " +_motor.MotorName;
+            motor.CurrentSpeed=_motor.CurrentSpeed;
+            return motor;
+        }
+    }
+}
---
+++ WcfServiceLibrary11111/iPLC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary11111
+{
+    [ServiceContract]
+    public interface IPLC
+    {
+        [OperationContract]
+        string GetDI(int value);
+
+        [OperationContract]
+        Motor GetMotor(Motor _motor);
+    }
+
+
+    [DataContract]
+    public class Motor
+    {
+        string motorName = "motor";
+        int currentSpeed=0;
+
+        [DataMember]
+        public string MotorName
+        {
+            get { return motorName; }
+            set { motorName = value; }
+        }
+
+        [DataMember]
+        public int CurrentSpeed
+        {
+            get { return currentSpeed; }
+            set { currentSpeed = value; }
+        }
+    }
+
+
+}
